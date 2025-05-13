34286288d0e734b63ebac908ca47dc539a4a11e4
NoriTN <nori.tran-nguyen@ext.saint-gobain.com>
2025-05-13 09:53:03 +0200

    Update main.tf

--- storage/main.tf
+++ storage/main.tf
@@ -60,7 +60,7 @@ resource "azurerm_storage_account" "web" {
   tags = var.tags
 }
 
-resource "azurerm_storage_account" "web" {
+resource "azurerm_storage_account" "sql" {
   name                            = "${lower(module.naming_storage.storage_name)}sqlbackup"
   resource_group_name             = var.resource_group_name
   location                        = var.location
